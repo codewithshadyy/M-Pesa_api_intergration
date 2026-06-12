4e1beb60db8442a18e5c731940d2785c0838c95a
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-06-12 18:32:21 +0300

    Routed checkStatus endpoint

--- routes/mpesa.js
+++ routes/mpesa.js
@@ -2,9 +2,10 @@
 
 const express = require("express")
 const router = express.Router()
-const {makePayment, callback} = require("../controllers/mpesa")
+const {makePayment, callback, checkStatus} = require("../controllers/mpesa")
 router.post("/pay", makePayment)
 router.post("/callback", callback)
+router.get("/status/:checkoutRequestID",  checkStatus)
 
 
 module.exports = router
