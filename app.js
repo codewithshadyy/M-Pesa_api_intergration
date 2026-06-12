4d8fe1dff673163d1647f3505f79ffb678831ca5
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-06-12 11:26:54 +0300

    Listening to the APP

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express")
-
+const app = express()
 const connectDB = require("./config/db")
+
+const dotenv = require("dotenv")
+dotenv.config()
 
 // database connection
 connectDB()
+
+
+
+
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`App runnign on:http://localhost:${process.env.PORT}`)
+})
