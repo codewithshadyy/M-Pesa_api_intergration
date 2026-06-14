d590957b9abdba998e3d1ce06d246ed72bc15396
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-06-14 20:57:49 +0300

    login endpoint

--- controllers/accountControllers.js
+++ controllers/accountControllers.js
@@ -2,9 +2,10 @@
 const Person = require("../models/Account")
 const bcrypt = require("bcrypt")
 const {generateToken} = require("../utils/generateToken")
+const account = require("../models/Account")
 
 
-exports.Register = async (req,res) => {
+exports.register = async (req,res) => {
 
     try {
 
@@ -43,6 +44,43 @@ exports.Register = async (req,res) => {
         
     } catch (error) {
 
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+        
+    }
+    
+}
+
+exports.login = async (req,res) => {
+
+    try {
+
+        const{username, password} = req.body
+        const user = await Person.findOne({username})
+
+        if(!user){
+            return res.status(404).json({
+                success:false,
+                message:"Invalid credenntials"
+            })
+        }
+
+        passwordMatch = bcrypt.compare(user.password)
+
+        const token = await generateToken(user)
+        return res.status(200).json({
+            success:true,
+            message:`welcome back ${user.username}!!!!`,
+            data:{
+                token,
+                id:user._id,
+                username:user.username
+            }
+        })
+        
+    } catch (error) {
         return res.status(500).json({
             success:false,
             message:error.message
