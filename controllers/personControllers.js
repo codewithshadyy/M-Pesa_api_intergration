eca8b2d727e184e5d48d70da8fd84207f3e04cad
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-06-14 19:11:52 +0300

    Created register endpoint

---
+++ controllers/personControllers.js
@@ -0,0 +1,53 @@
+
+const Person = require("../models/Person")
+const bcrypt = require("bcrypt")
+const {generateToken} = require("../utils/generateToken")
+
+
+exports.Register = async (req,res) => {
+
+    try {
+
+        const {username, email, password} = req.body
+        const personExists = await Person.findone({
+            $or:[{username}, {email}]
+        })
+
+        if(personExists){
+            return res.status(500).json({
+                success:false,
+                message:"OOp! try another email or password"
+            })
+        }
+
+        const hashedPassword = bcrypt.hash(password, 10)
+        const person = await Person.create({
+            username,
+            email,
+            password:hashedPassword
+        })
+
+        const token = await generateToken(person)
+
+        return res.status(201).json({
+            success:true,
+            message:`${person.username} creatde successfully`,
+
+            data:{
+                id:person._id,
+                username:person.username,
+                email:person.email,
+                token:token
+        }
+        })
+        
+    } catch (error) {
+
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+        
+    }
+    
+}
